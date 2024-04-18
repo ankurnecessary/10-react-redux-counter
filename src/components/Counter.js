96f4ce4d87aaf7e69ddf1a109d64c3d0c44838f0
Ankur Gupta <ankurnecessary@gmail.com>
2024-04-19 04:15:22 +0930

    Using actions of redux using useDispatch() hook.

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,15 +1,29 @@
 import React from 'react'
-import { useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import classes from './Counter.module.css'
 
 const Counter = () => {
+  const dispatch = useDispatch()
   const counter = useSelector(state => state.counter)
   const toggleCounterHandler = () => {}
+
+  const incrementHandler = () => {
+    console.log('increment')
+    dispatch({ type: 'increment' })
+  }
+  const decrementHandler = () => {
+    console.log('decrement')
+    dispatch({ type: 'decrement' })
+  }
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
       <div className={classes.value}>{counter}</div>
+      <div>
+        <button onClick={incrementHandler}>Increment</button>
+        <button onClick={decrementHandler}>Decrement</button>
+      </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   )
