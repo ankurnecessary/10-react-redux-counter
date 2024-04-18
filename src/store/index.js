841eadb414c0ad2f512899ccfd99b8bf8efd58f4
Ankur Gupta <ankurnecessary@gmail.com>
2024-04-19 03:44:51 +0930

    Added store in the app.

---
+++ src/store/index.js
@@ -0,0 +1,17 @@
+import { createStore } from 'redux'
+
+function counterReducer (state = { counter: 0 }, action) {
+  if (action.type === 'increment') {
+    return { ...state, counter: state.counter++ }
+  }
+
+  if (action.type === 'decrement') {
+    return { ...state, counter: state.counter-- }
+  }
+
+  return state
+}
+
+const store = createStore(counterReducer)
+
+export default store
