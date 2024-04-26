97e2cadef6ac02ac4f2f011fb0ada744858b812b
Ankur Gupta <ankurnecessary@gmail.com>
2024-04-27 06:37:17 +0930

    Replaced existing store configuration with the one provided by @reduxjs/toolkit.

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,7 @@
-import { createStore } from 'redux'
-import { createSlice } from '@reduxjs/toolkit'
+import { configureStore, createSlice } from '@reduxjs/toolkit'
 
 const initialState = { counter: 0, showCounter: true }
-createSlice({
+const counterSlice = createSlice({
   name: 'counter',
   initialState,
   reducers: {
@@ -21,26 +20,8 @@ createSlice({
   }
 })
 
-const counterReducer = (state = initialState, action) => {
-  if (action.type === 'increment') {
-    return { ...state, counter: state.counter + 1 }
-  }
-
-  if (action.type === 'increase') {
-    return { ...state, counter: state.counter + action.amount }
-  }
-
-  if (action.type === 'decrement') {
-    return { ...state, counter: state.counter - 1 }
-  }
-
-  if (action.type === 'toggle') {
-    return { ...state, showCounter: !state.showCounter }
-  }
-
-  return state
-}
-
-const store = createStore(counterReducer)
+const store = configureStore({
+  reducer: counterSlice.reducer
+})
 
 export default store
