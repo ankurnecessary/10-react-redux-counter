f90c65073a8eb10dae359f6c8d39c7272ce6a4fc
Ankur Gupta <ankurnecessary@gmail.com>
2024-04-27 14:00:46 +0930

    Added authentication state store slice to handle authentication related behavior.

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,9 @@
 import { configureStore, createSlice } from '@reduxjs/toolkit'
 
-const initialState = { counter: 0, showCounter: true }
+const initialCounterState = { counter: 0, showCounter: true }
 const counterSlice = createSlice({
   name: 'counter',
-  initialState,
+  initialState: initialCounterState,
   reducers: {
     increment (state) {
       state.counter++
@@ -20,9 +20,20 @@ const counterSlice = createSlice({
   }
 })
 
+const initialAuthState = { isAuthenticated: false }
+const authSlice = createSlice({
+  name: 'authentication',
+  initialState: initialAuthState,
+  reducers: {
+    logIn (state) { state.isAuthenticated = true },
+    logOut (state) { state.isAuthenticated = false }
+  }
+})
+
 const store = configureStore({
-  reducer: counterSlice.reducer
+  reducer: { counter: counterSlice.reducer, auth: authSlice.reducer }
 })
 
 export const counterActions = counterSlice.actions
+export const authActions = authSlice.actions
 export default store
